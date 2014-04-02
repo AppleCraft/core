903bfeb8a250dc66ffa4753d727822069343b2f7
Phrrrrrrroa <nr.phroa@gmail.com>
2014-04-02 10:53:28 -0700

    Implement a configuration
    nothing fancy in it

--- src/main/java/com/applecraftserver/plugins/core/Main.java
+++ src/main/java/com/applecraftserver/plugins/core/Main.java
@@ -5,19 +5,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
-@SuppressWarnings("unused")
 public class Main extends JavaPlugin {
-    private Main instance;
     private Logger logger = this.getLogger();
     private PluginDescriptionFile description;
+    private Configuration config;
 
     public void onEnable() {
         description = this.getDescription();
-        instance = this;
+        config = new Configuration(this);
+        __("Configuration initialized");
         _("Fully enabled v" + description.getVersion());
     }
 
     public void _(String msg) {
         logger.info(msg);
+    }
+
+    public void __(String msg) {
+        if (Configuration.debug)
+            logger.fine(msg);
     }
 }
