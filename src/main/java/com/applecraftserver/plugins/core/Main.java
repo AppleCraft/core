c61acd8558a7ed6484d4afa77d058fe2485ca9a3
Phrrrrrrroa <nr.phroa@gmail.com>
2014-04-01 22:09:13 -0700

    More main class

--- src/main/java/com/applecraftserver/plugins/core/Main.java
+++ src/main/java/com/applecraftserver/plugins/core/Main.java
@@ -1,12 +1,23 @@
 package com.applecraftserver.plugins.core;
 
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.logging.Logger;
 
-public class Main {
+@SuppressWarnings("unused")
+public class Main extends JavaPlugin {
     private Main instance;
     private Logger logger = this.getLogger();
+    private PluginDescriptionFile description;
 
     public void onEnable() {
+        description = this.getDescription();
         instance = this;
+        _("Fully enabled v" + description.getVersion());
+    }
+
+    public void _(String msg) {
+        logger.info(msg);
     }
 }
