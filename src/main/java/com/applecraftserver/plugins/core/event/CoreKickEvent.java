f87baee3501be41fd6f57ebd1c9b3ace4839a3be
Phrrrrrrroa <nr.phroa@gmail.com>
2014-04-01 18:32:36 -0700

    Moved the cheese
    ...to another branch. restarting because his code is stupid.

--- src/main/java/com/applecraftserver/plugins/core/event/CoreKickEvent.java
+++
@@ -1,8 +0,0 @@
-package com.applecraftserver.plugins.core.event;
-
-/**
- * Created by aprx on 2/1/14.
- */
-public abstract class CoreKickEvent extends ModeratorAction {
-
-}
--- src/main/java/com/applecraftserver/plugins/core/permissions/PermissionGroup.java
+++
@@ -1,7 +0,0 @@
-package com.applecraftserver.plugins.core.permissions;
-
-/**
- * Created by aeperiox on 2/7/14.
- */
-public class PermissionGroup {
-}
